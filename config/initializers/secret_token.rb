90e1908b3dae1678218302eb6b7480ca57e2efc9
satpai1215 <pai.satyan@gmail.com>
2014-02-03 22:56:17 -0800

    init on Soon Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Soon::Application.config.secret_token = '36de409f1ef1a1a6e82338696890f706ca2197a8556315475b2ea7a0226a58c0b98230075d0db7f80a7237c84e325e4438e5602b31742fad789bd3c04ff433f7'
