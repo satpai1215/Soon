a79b7cb439d67a5e485ca4a783c5b52e426c8b7e
satpai1215 <pai.satyan@gmail.com>
2014-03-04 23:00:04 -0800

    assets precompile false

--- config/application.rb
+++ config/application.rb
@@ -56,7 +56,7 @@ module Soon
     # Enable the asset pipeline
     config.assets.enabled = true
     config.assets.paths << "#{Rails.root}/vendor/assets/fonts"
-
+        config.assets.initialize_on_precompile = false
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
   end
